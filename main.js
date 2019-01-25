5af88c313962e316c2d9d71faeda6cbab969ff9c
Daniel Ahl <danielahl89@gmail.com>
2019-01-25 19:29:28 +0100

    js

--- main.js
+++ main.js
@@ -1,70 +1,36 @@
-let playing = false;
-let userName;
+let userIsPlaying = false;
 
+function startGame() {
+  var userName = document.getElementById("userName").value;
+  console.log(userName);
+  userIsPlaying = true;
+  console.log("Spelar? " + userIsPlaying);
 
-
-/* SPELET */
-
-function setGame() {
-
-    userName = document.getElementById("userName").value;
-    console.log(userName);
-    let currentLevel = 1;
-    let lifeLine = 1;
+  while(userIsPlaying) {
+    let userAnswer = confirm("Är du redo för första frågan?");
     
-    toggleShown("welcome-box","game-box");
-    playing = true;
-
-    while (playing) {
-    console.log(playing);
-    console.log("Spelet startat.");
-    // initiera variablerna som används i spelet.
-    // default är 1
-    console.log("Current level: " + currentLevel);
-    
-
+    if (userAnswer) {
+        console.log("Redo!");
+        toggleHidden("welcome-box", "game-box");
+        return;
+      } else {
+      console.log("Inte redo!");
+      userIsPlaying = false;
+      return userIsPlaying;
+    };
 
 
 
-    playing = false; //Stoppar while-loopen
-  };
+
+    //Avslutar spelet
+    userIsPlaying = false;
+    console.log("Spelar? " + userIsPlaying);
+  }
 };
 
-/* SPELET SLUT! */
-
-/* FUNKTIONER! */
-
-
-//Funktionen används för att dölja/visa element genom att sätta
-// .hidden på elementet.
-function toggleShown(id, id2) {
-  console.log("Toggle element with id='" + id + "'. ");
-  console.log("Toggle element with id='" + id2 + "'. ");
-  /* hämta elementet med id "id" */
-  let el = document.getElementById(id);
+function toggleHidden(id, id2) {
+  let el1 = document.getElementById(id);
   let el2 = document.getElementById(id2);
-  el.classList.toggle("hidden");
+  el1.classList.toggle("hidden");
   el2.classList.toggle("hidden");
-}
-
-function toggleChoice(id) {
-  let el = document.getElementById(id);
-  el.classList.toggle("hidden");
 };
-
-
-/* Används på knapp för att komma till nästa nivå. och generera nästa frågor. */
-function nextLevel() {
-  currentLevel += 1;
-  changeStage(currentLevel);
-};
-
-/* Funktionen tar ett argument, variabeln currentLevel och tar bort current från den gamla nivån och lägger till på nya nivån */
-function changeStage(lvl) {
-  let old = document.getElementsByClassName("current");
-  let next = document.getElementById(lvl);
-  old.toggle("current");
-  next.toggle("current");
-};
-
-//
